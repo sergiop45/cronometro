9d830a2308a42feebb2afa0ac9c396adf3639bc7
sergiop45 <sergiopsantos33@gmail.com>
2022-07-03 10:16:56 -0300

    cronos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 
-function App() {
+class App extends Component {
+
+constructor(props) {
+  super(props);
+
+  this.state = {
+    time: 0,
+    botao: "Iniciar"
+  }
+
+  this.iniciar = this.iniciar.bind(this);
+  this.zerar = this.zerar.bind(this);
+  this.time = null;
+}
+
+iniciar() {
+  
+
+  if(this.time !== null) {
+    clearInterval(this.time);
+    this.time = null;
+    this.setState({botao: "Iniciar"})
+  } else {
+    this.setState({botao: "Parar"})
+    this.time = setInterval(() => {
+      let state = this.state;
+      state.time += 0.1;
+      this.setState(state)
+    }, 100)
+  }
+
+}
+
+zerar() {
+  let state = this.state;
+  state.time = 0;
+  this.setState(state);
+}
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={require("./assets/cronometro.png")} className="img" />
+      <a className='timer'>{this.state.time.toFixed(1)}</a>
+      <div className='areaBtn'>
+        <a className='botao' onClick={this.iniciar}>{this.state.botao}</a>
+        <a className='botao' onClick={this.zerar}>Zerar</a>
+      </div>
     </div>
   );
+}
 }
 
 export default App;
